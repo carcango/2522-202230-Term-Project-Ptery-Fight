3bb59e63c49dffd4695a53bd9d859261d63a0351
rhysmah <97256979+rhysmah@users.noreply.github.com>
2022-11-12 16:10:15 -0800

    Updating menu via class, not FXML

--- src/main/java/ca/bcit/comp2522/termproject/secretwonders/GameMenu.java
+++ src/main/java/ca/bcit/comp2522/termproject/secretwonders/GameMenu.java
@@ -1,26 +1,52 @@
 package ca.bcit.comp2522.termproject.secretwonders;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class GameMenu extends Application {
-    private final int gameWindowHeight   = 311;
-    private final int gameWindowWidth    = 552;
-    private final String gameWindowTitle = "Secret Wonders";
+    private static final int BUTTON_SPACING = 10;
+
+    private final int    gameWindowHeight   = 300;
+    private final int    gameWindowWidth    = 400;
+    private final String gameWindowTitle    = "Secret Wonders";
 
     @Override
     public void start(final Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(GameMenu.class.getResource("MainMenuContent.fxml"));
+        // Game buttons
+        Button startGameButton = new Button();
+        startGameButton.setText("Start Game");
+        startGameButton.setOnAction(actionEvent ->
+                startGameButton.setText("Start game button pressed"));
 
-        Scene scene = new Scene(fxmlLoader.load(), gameWindowWidth, gameWindowHeight);
-        stage.setTitle(gameWindowTitle);
+        Button instructionsButton = new Button();
+        instructionsButton.setText("Instructions");
+        instructionsButton.setOnAction(actionEvent ->
+                instructionsButton.setText("Instructions button pressed"));
+
+        Button quitGameButton = new Button();
+        quitGameButton.setText("Quit Game");
+        quitGameButton.setOnAction(actionEvent ->
+                quitGameButton.setText("Quit Game button pressed"));
+
+        // Window layout
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(startGameButton, instructionsButton, quitGameButton);
+        vbox.setSpacing(BUTTON_SPACING);
+        vbox.setAlignment(Pos.CENTER);
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(vbox);
+
+        Scene scene = new Scene(borderPane, gameWindowWidth, gameWindowHeight);
         stage.setScene(scene);
+        stage.setTitle(gameWindowTitle);
         stage.show();
     }
 
