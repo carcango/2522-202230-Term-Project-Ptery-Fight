563186be1f29adb92bb1beabe7847c2dad53f2e5
Carson.O <carcango@gmail.com>
2022-11-23 08:47:55 -0800

    changed projectile type for player 2

--- src/main/java/ca/bcit/comp2522/termproject/secretwonders/GameEngine.java
+++ src/main/java/ca/bcit/comp2522/termproject/secretwonders/GameEngine.java
@@ -3,10 +3,10 @@ package ca.bcit.comp2522.termproject.secretwonders;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.util.Duration;
 
 import java.util.ArrayList;
+
 
 public class GameEngine {
 
@@ -18,16 +18,6 @@ public class GameEngine {
     private GamePane pane;
     private Scene scene;
     private Timeline gameLoop;
-
-    //Entities
-//    private final Image player1Sprite = new Image("bee.gif");
-//    private final Image player2Sprite = new Image("dragonfly.gif");
-//
-//    private Player player1 = new Player(player1Sprite, Constants.PLAYER_ONE_HEIGHT,
-//            Constants.PLAYER_ONE_WIDTH, Constants.PLAYER_ONE_HEALTH);
-//    private Player player2 = new Player(player2Sprite, Constants.PLAYER_TWO_HEIGHT,
-//            Constants.PLAYER_TWO_WIDTH, Constants.PLAYER_TWO_HEALTH
-//    );
 
     private Player1 player1 = new Player1();
     private Player2 player2 = new Player2();
