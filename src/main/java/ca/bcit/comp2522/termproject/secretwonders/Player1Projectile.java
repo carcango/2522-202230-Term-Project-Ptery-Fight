f7210f29dc1f0e01f93d2654c9a5a45f22fba628
Carson.O <carcango@gmail.com>
2022-11-25 20:14:29 -0800

    Added documentation

--- src/main/java/ca/bcit/comp2522/termproject/secretwonders/Player1Projectile.java
+++ src/main/java/ca/bcit/comp2522/termproject/secretwonders/Player1Projectile.java
@@ -1,12 +1,25 @@
 package ca.bcit.comp2522.termproject.secretwonders;
 
+/**
+ * Player1 projectile.
+ * @author Olafson and Mahannah
+ * @version 2022.
+ */
 public class Player1Projectile extends Projectile {
-
+    /**
+     * default constructor.
+     */
     public Player1Projectile() {
         super("short_spear.png", Constants.PROJECTILE_PLAYER_ONE_WIDTH,
                 Constants.PROJECTILE_PLAYER_ONE_HEIGHT, Constants.PROJECTILE_PLAYER_DMG);
     }
 
+    /**
+     * constructs projectile at a certain location with angle, speed, and size.
+     * @param originX starting X location.
+     * @param originY starting Y location.
+     * @param angle direction projectile is facing.
+     */
     public Player1Projectile(double originX, double originY, double angle) {
         super("short_spear.png", Constants.PROJECTILE_PLAYER_ONE_WIDTH,
                 Constants.PROJECTILE_PLAYER_ONE_HEIGHT,
--- src/main/java/ca/bcit/comp2522/termproject/secretwonders/Player2Projectile.java
+++ src/main/java/ca/bcit/comp2522/termproject/secretwonders/Player2Projectile.java
@@ -1,12 +1,23 @@
 package ca.bcit.comp2522.termproject.secretwonders;
-
+/**
+ * Player2 projectile.
+ * @author Olafson and Mahannah
+ * @version 2022.
+ */
 public class Player2Projectile extends Projectile {
-
+    /**
+     * default constructor.
+     */
     public Player2Projectile() {
         super("smokeyFireball.gif", Constants.PROJECTILE_PLAYER_TWO_WIDTH,
                 Constants.PROJECTILE_PLAYER_TWO_HEIGHT, Constants.PROJECTILE_PLAYER_DMG);
     }
-
+    /**
+     * constructs projectile at a certain location with angle, speed, and size.
+     * @param originX starting X location.
+     * @param originY starting Y location.
+     * @param angle direction projectile is facing.
+     */
     public Player2Projectile(double originX, double originY, double angle) {
         super("smokeyFireball.gif", Constants.PROJECTILE_PLAYER_TWO_WIDTH,
                 Constants.PROJECTILE_PLAYER_TWO_HEIGHT, Constants.PROJECTILE_PLAYER_DMG,
