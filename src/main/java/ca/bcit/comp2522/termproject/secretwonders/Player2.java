54eeb29bc3fdf4bce34af1e0375e2d172866113e
Carson.O <carcango@gmail.com>
2022-11-25 20:04:29 -0800

    Added documentation

--- src/main/java/ca/bcit/comp2522/termproject/secretwonders/Player2.java
+++ src/main/java/ca/bcit/comp2522/termproject/secretwonders/Player2.java
@@ -1,25 +1,52 @@
 package ca.bcit.comp2522.termproject.secretwonders;
 
 import javafx.geometry.Point2D;
-import javafx.scene.image.Image;
 import javafx.scene.transform.Rotate;
-
+/**
+ * Player Two Class.
+ * @author Olafson and Mahannah.
+ * This is the Dragonfly character that is controlled with the WASD keys and Space Bar.
+ */
 public class Player2 extends Character {
-
+    /**
+     * how fast the character moves and turns.
+     */
     final int movementFactor = Constants.PLAYER_TWO_MOVEMENT;
+    /**
+     * monitors if the flag for turning left is on.
+     */
     protected boolean turnLeft = false;
+    /**
+     * monitors if the flag for turning right is on.
+     */
     protected boolean turnRight = false;
+    /**
+     * monitors if the flag for going forward is on.
+     */
     protected boolean goForward = false;
+    /**
+     * monitors if the flag for going backward is on.
+     */
     protected boolean goBackward = false;
+    /**
+     * creates rotate object for player.
+     */
     final Rotate rotatePlayer = new Rotate();
+    /**
+     * Creates initial Direction of directly up.
+     */
     Point2D initialDirection = new Point2D(0, -1);
-
+    /**
+     * Constructs player two.
+     */
     public Player2() {
         super("dragonfly.gif", Constants.PLAYER_TWO_WIDTH,
                 Constants.PLAYER_TWO_HEIGHT, Constants.PLAYER_TWO_HEALTH);
         setInitialPosition();
     }
-
+    /**
+     * Sets initial spawn location and sets the rotate object to the player to allow rotation Transformations.
+     */
     private void setInitialPosition() {
         setX(0);
         setY(Constants.SCREEN_HEIGHT - Constants.PLAYER_TWO_HEIGHT);
@@ -27,7 +54,10 @@ public class Player2 extends Character {
         rotatePlayer.setPivotY(getCenterY());
         getTransforms().addAll(rotatePlayer);
     }
-
+    /**
+     * sets the movement booleans to true.
+     * @param dir Up, down, left or right.
+     */
     public void startMovement(GameEngine.Direction dir) {
         switch (dir) {
             case UP:
@@ -40,7 +70,10 @@ public class Player2 extends Character {
                 turnRight = true; break;
         }
     }
-
+    /**
+     * stops player movements when key press has been released.
+     * @param dir UP, DOWN, LEFT, or RIGHT.
+     */
     public void stopMovement(GameEngine.Direction dir) {
         switch (dir) {
             case UP -> goForward = false;
@@ -49,7 +82,11 @@ public class Player2 extends Character {
             case RIGHT -> turnRight = false;
         }
     }
-
+    /**
+     * Moves the player two one step forward or backward.
+     * @param changeInX how much the player is supposed to move along relative X coordinate (if turning).
+     * @param changeInY how much the player is supposed to move along relative Y coordinate (forward or backwards).
+     */
     private void movePlayer(final double changeInX, final double changeInY) {
         if (changeInX == 0 && changeInY == 0) {
             return;
@@ -59,7 +96,11 @@ public class Player2 extends Character {
         setX(x);
         setY(y);
     }
-
+    /**
+     * Interprets instructions to move Player two.
+     * Sets the pivot to centre of player, increments or rotates the movementChange based on what booleans are true,
+     * and transforms the player in that direction.
+     */
     @Override
     public void doMovement() {
         rotatePlayer.setPivotX(getCenterX());
@@ -85,6 +126,11 @@ public class Player2 extends Character {
         movePlayer(pt1.getX(), pt1.getY());
 
     }
+    /**
+     * rotates player one.
+     * @param angle how much to rotate player one.
+     * if angle is 360, set back to zero. (full rotation)
+     */
     public void rotatePlayer(float angle) {
         angle += rotatePlayer.getAngle();
         if (angle == 360) {
@@ -92,7 +138,9 @@ public class Player2 extends Character {
         }
         rotatePlayer.setAngle(angle);
     }
-
+    /**
+     * creates Player two Projectile at characters location.
+     */
     public void fireProjectile() {
         ((GamePane) getParent()).getEngine().queueAddition(
                 new Player2Projectile(
